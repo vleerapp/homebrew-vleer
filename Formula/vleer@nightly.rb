5d1a141c3e71f18472409b6c858bddaa06205dd0
PandaDEV <pandadev.net@proton.me>
2026-09-06 20:38:07 +0200

    Update vleer@nightly.rb

--- Formula/vleer@nightly.rb
+++ Formula/vleer@nightly.rb
@@ -12,12 +12,12 @@ class VleerATNightly < Formula
   sha256 Hardware::CPU.arm? ? "ce5805aab478ae6ba0eaa8de195304f19410822b234129c0cea173678b5b608e" : "833b0e9e80f8a81fcd3a450c2eb19df76500b5bb8f7745a6f85c50b40090d38d"
 
   def install
-    bin.install "bin/vleer" => "vleer-nightly"
+    bin.install "bin/vleer"
     share.install "share/applications", "share/icons"
     prefix.install "share/vleer/LICENSE"
   end
 
   test do
-    system "#{bin}/vleer-nightly", "--version"
+    system "#{bin}/vleer", "--version"
   end
 end
